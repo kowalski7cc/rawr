4be91f70e9a0d0e298275451c9f3c34201b32f7e
Kowalski Dragon (kowalski7cc) <kowalski7cc@users.noreply.github.com>
2023-10-21 19:29:51 +0200

    Add emoji version
    Signed-off-by: Kowalski Dragon (kowalski7cc) <kowalski7cc@users.noreply.github.com>

--- rawr.fish
+++ rawr.fish
@@ -1,12 +1,11 @@
 function rawr --description "A command-line system information tool written in fish"
-    read name other linux_version_string info </proc/version
+    argparse -n rawr -i 'e/emoji' -- $argv
+    read name version_text linux_version_string info </proc/version
     set kernel (string split -r -m1 \- $linux_version_string)[1]
-
-    set cros 
 
     if string match -q "*Chromium OS*" $info
         set -f NAME "Chrome OS"
-        set -f XDG_CURRENT_DESKTOP "aura shell" 
+        set -f XDG_CURRENT_DESKTOP "Aura Shell" 
     else if test -r /etc/os-release
         for row in (read -z < /etc/os-release)
             set -l kv (string split -m 1 \= $row)
@@ -16,13 +15,20 @@ function rawr --description "A command-line system information tool written in f
 
     echo (set_color -o red)"  os"(set_color normal)" ~ $NAME $VERSION_ID"
     echo (set_color -o red)"  sh"(set_color normal)" ~ fish $FISH_VERSION"
-    echo (set_color -o red)"  de"(set_color normal)" ~ "(string lower $XDG_CURRENT_DESKTOP)
-    echo (set_color -o red)"  kn"(set_color normal)" ~ "(string split -m1 \- $kernel)[1]
-    echo ""
-    echo "        . "
-    echo "   .>   )\;`a__"
-    echo "  (  _ _)/ /-.\" "(set_color -o red)"~~"(set_color normal)
-    echo "   `( )_ )/"
-    echo "    <_  <_     Welcome to "(prompt_hostname)", "(set_color red)"$USER"(set_color normal)
-    echo ""
+    if set -q "XDG_CURRENT_DESKTOP"
+        echo (set_color -o red)"  de"(set_color normal)" ~ "(string lower $XDG_CURRENT_DESKTOP)
+    end
+    printf (set_color -o red)"  kn"(set_color normal)" ~ "(string split -m1 \- $kernel)[1]"\n\n"
+    if test "$_flag_emoji" = "--emoji"; or test "$_flag_emoji" = "-e"
+        # TODO: Check if fish_emoji_width is 2
+        printf "  🐲 ~ Welcome to "(prompt_hostname)", "(set_color red)"$USER"(set_color normal)"\n\n"
+    else
+        echo "        . "
+        echo "   .>   )\;`a__"
+        echo "  (  _ _)/ /-.\" "(set_color -o red)"~~"(set_color normal)
+        echo "   `( )_ )/"
+        echo "    <_  <_     Welcome to "(prompt_hostname)", "(set_color red)"$USER"(set_color normal)
+        echo ""
+    end
+
 end
